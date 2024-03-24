2263ea458b9a6e3ad437053dce53e37a3ab20d1b
ohansck <ohansck@gmail.com>
2024-03-24 23:59:15 +0100

    modify pipeline

--- bin/infra.ts
+++ bin/infra.ts
@@ -26,12 +26,6 @@ new PipelineStack(app, 'IBankPipelineStack', {});
 // })
 
 // new InfraStack(app, 'IBankStack', {
-
-
-//   /* If you don't specify 'env', this stack will be environment-agnostic.
-//    * Account/Region-dependent features and context lookups will not work,
-//    * but a single synthesized template can be deployed anywhere. */
-
 //   /* Uncomment the next line to specialize this stack for the AWS Account
 //    * and Region that are implied by the current CLI configuration. */
 //   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
